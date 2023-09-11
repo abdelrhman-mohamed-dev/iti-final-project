c4ca7c620053c9e5bf8d65e78e7f954f5c470a4e
boody-cpu <81966035+boody-cpu@users.noreply.github.com>
2023-09-11 20:36:56 +0300

    final

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { lazy, Suspense } from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+import NotFound from "./pages/NotFound";
+import { MovieContextProvider } from "./context/MovieContext";
+import { Loading } from "./components/Loading";
+import ShowTvInfo from "./helpers/ShowTvInfo";
+import Search from "./pages/Search";
+
+const Home = lazy(() => import("./pages/Home"));
+const Movies = lazy(() => import("./pages/Movies"));
+const TvSeries = lazy(() => import("./pages/TvSeries"));
+const ShowInfo = lazy(() => import("./pages/ShowInfo"));
+const Favorite = lazy(() => import("./pages/Favorite"));
+
+export default function App() {
+  return (
+    <>
+      <MovieContextProvider>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/tv-series" element={<TvSeries />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/favorite" element={<Favorite />} />
+            <Route path="/:id" element={<ShowInfo />} />
+            <Route path="/tv/:id" element={<ShowTvInfo />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </MovieContextProvider>
+    </>
+  );
+}
